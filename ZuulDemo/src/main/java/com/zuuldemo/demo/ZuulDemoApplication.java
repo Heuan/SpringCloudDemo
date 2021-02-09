66cd7b5d2b929924c7c8d305eddda4da396ffaa1
heyuan <303565317@qq.com>
2021-02-09 10:07:21 +0800

    hystrix:熔断 hystrix-dashboard：熔断监控面板

--- ZuulDemo/src/main/java/com/zuuldemo/demo/ZuulDemoApplication.java
+++ ZuulDemo/src/main/java/com/zuuldemo/demo/ZuulDemoApplication.java
@@ -2,10 +2,15 @@ package com.zuuldemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ *  监控 /actuator/hystrix.stream
+ */
 @SpringBootApplication(scanBasePackages = {"com.zuuldemo.demo.*"})
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class ZuulDemoApplication {
 
     public static void main(String[] args) {
